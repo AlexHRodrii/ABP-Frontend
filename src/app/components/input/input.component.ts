b170860ac3982440408c993e1030bc02a63e4f3e
Daniel Luengo <danielluengoecheverria@gmail.com>
2023-03-15 16:22:45 +0100

    Prop en ingles, div eliminado

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,18 +1,17 @@
 import { Component, Input } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
 export class InputComponent {
-  @Input() etiqueta: string = '';
+  @Input() title: string = '';
+  @Input() name: string = '';
   @Input() placeholder: string = '';
-  @Input() tipo: string = '';
-  @Input() validacion?: string = '';
+  @Input() type: string = '';
+  @Input() validation?: string = '';
 
   /*validar(event: Event) {
-    return this.validacion.test("a");
+    return this.validation.test("a");
   }*/
 }
