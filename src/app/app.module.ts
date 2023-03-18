80740992ccc49130eef54fce0168308bf269030c
Daniel Luengo <danielluengoecheverria@gmail.com>
2023-03-18 14:07:31 +0100

    Vista detalle instalaccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BotonComponent } from './components/boton/boton.component';
 import { InputComponent } from './components/input/input.component';
 import { TextoComponent } from './components/texto/texto.component';
+import { InstalacionComponent } from './components/instalacion/instalacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BotonComponent
-    TextoComponent
-    InputComponent
+    BotonComponent,
+    TextoComponent,
+    InputComponent,
+    InstalacionComponent
   ],
   imports: [
     BrowserModule,
