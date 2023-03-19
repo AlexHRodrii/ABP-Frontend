dee19d2f8ef6dc564ac9a3f14df69a6576e10546
Alejandro Hernández Rodríguez <ahr24@alu.ua.es>
2023-03-19 21:26:08 +0100

    Add routes for all views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from '@angular/core';
-import {Router, RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./views/home/home.component";
 import {LoginComponent} from "./views/login/login.component";
 import {RegisterComponent} from "./views/register/register.component";
 import {ResumenComponent} from "./views/resumen/resumen.component";
+import {ListaInstalacionesComponent} from "./views/lista-instalaciones/lista-instalaciones.component";
+import {ListaProductosComponent} from "./views/lista-productos/lista-productos.component";
+import {InstalacionComponent} from "./views/instalacion/instalacion.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'cart', component: ResumenComponent},
+  {
+    path: 'instalaciones', component: ListaInstalacionesComponent, children: [
+      {path: ':id', component: InstalacionComponent},
+    ]
+  },
+  {path: 'productos', component: ListaProductosComponent},
 ];
 
 @NgModule({
