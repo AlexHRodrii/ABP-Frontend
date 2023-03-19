fb898435729d40039383d984634b0ab1384b2516
Alejandro Hernández Rodríguez <ahr24@alu.ua.es>
2023-03-19 23:39:28 +0100

    Design improvements

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-menu',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
   public itemsInCart: number = 0
-  public isUserLogged: boolean = true
+  public isUserLogged: boolean = localStorage.getItem('isLogged') === 'true'
+
+  constructor(private router: Router) {
+  }
+
+  logout() {
+    localStorage.removeItem('isLogged')
+    this.router.navigate(['/'])
+  }
 }
